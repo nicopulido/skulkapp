b1fbcb8e86e58908e3c2896282da2e6f1ae9ddee
nicopulido <nicopipe2010@gmail.com>
2026-02-05 18:36:44 -0500

    added documentation to MainActivity.kt

--- app/src/main/java/com/me/freeskullkingscorer/MainActivity.kt
+++ app/src/main/java/com/me/freeskullkingscorer/MainActivity.kt
@@ -12,8 +12,17 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.button.MaterialButton
 
+
+/*
+Principal screen of the app
+Allows:
+    create the players of the game
+    show the players on a list
+    move to the game screen
+ */
 class MainActivity : AppCompatActivity() {
 
+    //Not mutable list of players names
     private var listItems = ArrayList<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,21 +35,26 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        //Configuration of listview and adapter
         val adapter = ArrayAdapter(this,android.R.layout.simple_list_item_1,listItems)
         val listView = findViewById<ListView>(R.id.listOfPlayers)
         listView.adapter = adapter
 
-        val button = findViewById<ImageButton>(R.id.addPlayersButton)
-        button.setOnClickListener {
+
+        //button to add a new player to the list
+        val addPlayersButton = findViewById<ImageButton>(R.id.addPlayersButton)
+        addPlayersButton.setOnClickListener {
             val editText = findViewById<EditText>(R.id.enterNameEditText)
             val text = editText.text.toString()
             if(text.isNotEmpty()){
+                //adds a new player to the beginning of the list
                 listItems.add(0,text)
                 adapter.notifyDataSetChanged()
                 editText.text.clear()
             }
         }
 
+        //button to start the game
         val startButton = findViewById<MaterialButton>(R.id.buttonStart)
         startButton.setOnClickListener {
             val intent : Intent = Intent(this, inPlayScreen::class.java)
