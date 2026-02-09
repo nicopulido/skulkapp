ed24485092097eee52addcc5702c4e2474cea3f8
nicopulido <nicopipe2010@gmail.com>
2026-02-09 11:28:06 -0500

    added playerID, list of points and changed methods to change points

--- app/src/main/java/com/me/freeskullkingscorer/Player.kt
+++ app/src/main/java/com/me/freeskullkingscorer/Player.kt
@@ -1,10 +1,16 @@
 package com.me.freeskullkingscorer
 
+import java.util.UUID
+
 
 //This is the class that contains the player data
 data class Player(
+    //immutable player id
+    val playerID: String = UUID.randomUUID().toString(),
     //immutable player name
     val playerName: String,
+    //Points of a player per round
+    val pointsPerRound: MutableList<Int> = mutableListOf(),
     //mutable player score
     var playerScore: Int = 0,
 
@@ -13,10 +19,12 @@ data class Player(
     //methods to add or substract points to the player
 
     fun addPoints(points : Int){
+        pointsPerRound.add(points)
         playerScore += points
     }
 
     fun substractPoints(points : Int){
+        pointsPerRound.add(points)
         playerScore -= points
     }
 }
