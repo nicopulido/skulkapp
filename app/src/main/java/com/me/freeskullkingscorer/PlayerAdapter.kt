e5efa49b454ae37e413c8406032b5e09016b254f
nicopulido <nicopipe2010@gmail.com>
2026-02-10 18:38:14 -0500

    chore(ui): scaffold player adapter and list item layout

--- app/src/main/java/com/me/freeskullkingscorer/PlayerAdapter.kt
+++ app/src/main/java/com/me/freeskullkingscorer/PlayerAdapter.kt
@@ -1,4 +1,30 @@
 package com.me.freeskullkingscorer
 
-class PlayerAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.TextView
+
+class PlayerAdapter(
+    context: Context,
+    //source of real data
+    private val players: MutableList<Player>
+
+) : ArrayAdapter<Player>(context, 0, players) {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        val view = convertView ?: LayoutInflater.from(context)
+            .inflate(R.layout.list_item_player, parent, false)
+
+        val player = players[position]
+
+        val nameText = view.findViewById<TextView>(R.id.playerNameListText)
+        nameText.text = player.playerName
+
+        return super.getView(position, convertView, parent)
+    }
+
 }
